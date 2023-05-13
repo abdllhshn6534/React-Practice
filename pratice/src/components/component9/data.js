1459ad12470d0cd997c3d9704b6a4637ae08c3aa
abdullah <abdllhshn6534@gmail.com>
2023-05-13 18:07:30 +0300

    son hatalar düzeltildi

--- pratice/src/components/component9/data.js
+++ pratice/src/components/component9/data.js
@@ -1,5 +1,5 @@
 const yazi = [
-    {
+   
       ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae temporibus nihil magni assumenda odio illum, nam vero! Nam quidem quisquam a perspiciatis minima perferendis soluta deserunt labore totam, doloribus aut`,
       ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae temporibus nihil magni assumenda odio illum, nam vero! Nam quidem quisquam a perspiciatis minima perferendis soluta deserunt labore totam, doloribus aut`,
       ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae temporibus nihil magni assumenda odio illum, nam vero! Nam quidem quisquam a perspiciatis minima perferendis soluta deserunt labore totam, doloribus aut`,
@@ -12,6 +12,6 @@ const yazi = [
       ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae temporibus nihil magni assumenda odio illum, nam vero! Nam quidem quisquam a perspiciatis minima perferendis soluta deserunt labore totam, doloribus aut`,
 
          
-    }
+    
 ]
 export default yazi;
--- pratice/src/components/component9/paragraf.js
+++ pratice/src/components/component9/paragraf.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import data from "./data"
+
 const Paragraf = () => {
-    const [count, setcount] = useState(0);
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([])
+    /*nfn fonksiyonun kısaltmasıdır*/
+    const handleGenerate = (e) => {
+        e.preventDefault();
+        let amount = count;
+        if (count <=0) {
+            amount = 1;
+        }
+        if(count>data.length){
+            amount=data.length;
+        }
+        setText(data.slice(0,amount));
+
+    }
     return (
         <div>
             <h3>Kaç tane paragraf</h3>
             <form action="">
                 <label htmlFor="amount"></label>
-                <input type="number" id="amount" />
-                <button className='btn primary'>Generate</button>
+                <input type="number" id="amount" value={count} onChange={(e) => setCount(e.target.value)} />
+                <button className='btn ' onClick={handleGenerate}>Generate</button>
                 <button className='btn btn-danger'>Clear</button>
-
             </form>
             <div className="lorem">
-
+                {
+                    text.map((item, index)=>(<p key={index}>{item}</p>))
+                }
             </div>
         </div>
     )
